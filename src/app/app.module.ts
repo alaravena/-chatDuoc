81d518694bac0ff129280761138e13d3dbaa61d3
Alejandra Aravena Silva <al.aravena@profesor.duoc.cl>
2021-06-26 13:09:00 -0400

    chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,18 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+const config: SocketIoConfig = { url: 'https://chat-duoc-2021.herokuapp.com/', options: {} };
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    SocketIoModule.forRoot(config)
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  message = '';
+  messages = [];
+  currentUser = '';
+
+  constructor(private socket: Socket, private toastCtrl: ToastController) {}
+
+  ngOnInit() {
+    this.socket.connect();
+
+    const name = `user-${new Date().getTime()}`;
+    this.currentUser = name;
+
+    this.socket.emit('set-name', name);
+
+    this.socket.fromEvent('users-changed').subscribe(data => {
+        const user = data['user'];
+        if (data['event'] === 'left') {
+          this.showToast('Abandonó el chat: ' + user);
+        } else {
+          this.showToast('Se unió al chat: ' + user);
+        }
+    });
+
+    this.socket.fromEvent('message').subscribe(message => {
+        this.messages.push(message);
+    });
+  }
+
+  sendMessage() {
+    this.socket.emit('send-message', { text: this.message });
+    this.message = '';
+  }
+
+  ionViewWillLeave() {
+    this.socket.disconnect();
+  }
+
+  async showToast(msg) {
+    const toast = await this.toastCtrl.create({
+      message: msg,
+      position: 'top',
+      duration: 2000
+    });
+    toast.present();
+  }
+
 
 }
